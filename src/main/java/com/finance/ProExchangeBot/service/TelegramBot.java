7b526a73b167eed668df98d33a1c044bd2093aed
Vladimir008 <vladimir.podkidyshev@gmail.com>
2022-10-26 22:05:09 +0300

    Add DB

--- src/main/java/com/finance/ProExchangeBot/service/TelegramBot.java
+++ src/main/java/com/finance/ProExchangeBot/service/TelegramBot.java
@@ -1,23 +1,53 @@
 package com.finance.ProExchangeBot.service;
 
 import com.finance.ProExchangeBot.config.BotConfig;
+import com.finance.ProExchangeBot.model.User;
+import com.finance.ProExchangeBot.model.UserRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.commands.SetMyCommands;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.commands.BotCommand;
+import org.telegram.telegrambots.meta.api.objects.commands.scope.BotCommandScopeDefault;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.List;
 
 @Slf4j
 @Component
 public class TelegramBot extends TelegramLongPollingBot {
 
     @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
     final BotConfig config;
+
+    static final String HELP_TEXT = "This bot is created to demonstrate Spring capabilities.\n\n" +
+            "You can execute commands from the main menu on the left or by typing a command:\n\n" +
+            "Type /start to see a welcome message\n\n" +
+            "Type /help to see this message again\n\n" +
+            "Type /mydata to get your data stored\n\n" +
+            "Type /setting to set your preferences";
 
     public TelegramBot(BotConfig config) {
         this.config = config;
+        List<BotCommand> listOfCommands = new ArrayList<>();
+        listOfCommands.add(new BotCommand("/start", "get a welcome message!"));
+        listOfCommands.add(new BotCommand("/help", "info how to use this bot"));
+        listOfCommands.add(new BotCommand("/mydata", "get your data stored"));
+        listOfCommands.add(new BotCommand("/setting", "set your preferences"));
+        try {
+            this.execute(new SetMyCommands(listOfCommands, new BotCommandScopeDefault(), null));
+        } catch (TelegramApiException e) {
+            log.error("Error setting bot's command list " + e.getMessage());
+        }
     }
 
     @Override
@@ -37,19 +67,50 @@ public class TelegramBot extends TelegramLongPollingBot {
             long chatId = update.getMessage().getChatId();
 
             switch (message) {
+                
                 case "/start":
+                    
+                    registerUser(update.getMessage());
                     startCommandReceived(chatId, update.getMessage().getChat().getFirstName());
                     break;
+
+                case "/help":
+                    
+                    sendMessage(chatId, HELP_TEXT);
+                    break;
+
                 default:
+                    
                     sendMessage(chatId, "Sorry, command was not recognized!");
                     break;
             }
         }
     }
 
+    private void registerUser(Message msg) {
+
+        if(userRepository.findById(msg.getChatId()).isEmpty()) {
+
+            var chatId = msg.getChatId();
+            var chat = msg.getChat();
+
+            User user = new User();
+
+            user.setChatId(chatId);
+            user.setFirstName(chat.getFirstName());
+            user.setLastName(chat.getLastName());
+            user.setUserName(chat.getUserName());
+            user.setRegisteredAt(new Timestamp(System.currentTimeMillis()));
+
+            userRepository.save(user);
+            log.info("User safed  " + user);
+
+        }
+    }
+
     private void startCommandReceived(long chatID, String name) {
-        String answer = "\uD83D\uDC4BРџСЂРёРІРµС‚, " + name + "! РЇ ProObmenBot. " +
-                "Р§С‚РѕР±С‹ РЅР°С‡Р°С‚СЊ СЃРѕ РјРЅРѕР№ СЂР°Р±РѕС‚Р°С‚СЊ РЅР°Р¶РјРё РЅР° СЃР»РѕРІРѕ В«РЎРўРђР РўВ»";
+        String answer = "\uD83D\uDC4BПривет, " + name + "! Я ProObmenBot. " +
+                "Чтобы начать со мной работать нажми на слово «СТАРТ»";
         log.info("Replied to user " + name );
         sendMessage(chatID, answer);
     }
